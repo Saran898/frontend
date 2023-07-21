d262784b96f479802a5a711f238afdc6d8aa7cc8
saran898 <saranyu.raghupathy.securrahealth.com>
2023-07-21 17:44:39 +0530

    chnaged button title

--- src/page/dashboard/Header.js
+++ src/page/dashboard/Header.js
@@ -9,7 +9,7 @@ function Header(setIsAdding) {
         <div>
             
         <button onClick={() => setIsAdding(true)}
-        className='round-button'> Add Button</button>
+        className='round-button'> Add Employee</button>
 
          </div> 
         
